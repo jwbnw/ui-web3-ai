681df9c079819640964e7dace7daae0efc987a25
jwbnw <32151160+jwbnw@users.noreply.github.com>
2023-09-15 10:11:20 -0700

    #10: Handle component swap from sidebar

--- src/components/PlaygroundSideBar.tsx
+++ src/components/PlaygroundSideBar.tsx
@@ -2,9 +2,13 @@ import Link from 'next/link';
 import { FC, useState } from 'react';
 
 
-export const PlaygoundSideBar: React.FC = () => {
+export const PlaygoundSideBar: React.FC<any> = (props) => {
     const [open, setOpen] = useState(false);
-   
+    
+    {/*Couple these at somepoint*/}
+    const AiArtNameConst = "AiArtComponent";
+    const AiTextNameConst = "AiTextComponent";
+
     return (
         <div
           className={` ${
@@ -34,48 +38,20 @@ export const PlaygoundSideBar: React.FC = () => {
             <div className="flex-1">
               <ul className="pt-2 pb-4 space-y-1 text-sm">
                 <li className="rounded-sm">
-                  <Link
-                    href="playground"
+                  <button
+                    onClick={() => props.sendAiComponentData(AiArtNameConst)}
                     className="flex items-center p-2 space-x-3 rounded-md"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-gray-100"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                      />
-                    </svg>
                     <span className="text-gray-100">AI Art Generator</span>
-                  </Link>
+                  </button>
                 </li>
                 <li className="rounded-sm">
-                  <Link
-                    href="/playground"
+                  <button
+                    onClick={() => props.sendAiComponentData(AiTextNameConst)}
                     className="flex items-center p-2 space-x-3 rounded-md"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-gray-100"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                      />
-                    </svg>
                     <span className="text-gray-100">AI Text Generator</span>
-                  </Link>
+                  </button>
                 </li>
               </ul>
             </div>
--- src/views/playground/index.tsx
+++ src/views/playground/index.tsx
@@ -7,16 +7,21 @@ import { AiArtComponent } from "components/AiArtComponent";
 import { AiTextComponent } from "components/AiTextComponent";
 
 export const PlaygroundView: FC = ({}) => {
-  const renderArt = true;
+  const [aiCompoentToRender, setAiComonentToRender] = useState("AiArtComponent");
 
+  const sendAiComponentData = (aiCompoentToRender) => {
+    console.log("made it to sendData", aiCompoentToRender);
+    setAiComonentToRender(aiCompoentToRender);
+  }
+  //TODO: Render AI Component based on sidebar choice then done with this ticket!
   return (
     <div className="w-screen">
       <div className="flex-row lg:flex">
-        <PlaygoundSideBar/>
+        <PlaygoundSideBar sendAiComponentData={sendAiComponentData}/> 
         <div className="container mx-auto mt-4 lg:mt-12 p-4 shadow-sm">
           {/*This down can also go in the seperate playgound components*/}
             <div className="md:hero mx-auto p-4">
-              { renderArt ? < AiArtComponent /> : < AiTextComponent /> }
+              { aiCompoentToRender === 'AiArtComponent' ? < AiArtComponent /> : < AiTextComponent /> }
             </div>
         </div>
     </div>
