d2842af132877189f8cb2324a25456a2787cbc57
jwbnw <32151160+jwbnw@users.noreply.github.com>
2023-10-04 16:05:54 -0700

    #27: Resets playgroudsidebar style

--- src/components/PlaygroundSideBar.tsx
+++ src/components/PlaygroundSideBar.tsx
@@ -10,11 +10,11 @@ export const PlaygoundSideBar: React.FC<any> = (props) => {
     const AiTextNameConst = "AiTextComponent";
 
     return (
-        <div
-          className={` ${
-            open ? "lg:w-6" : "lg:w-60 "
-          }left-0 flex flex-col p-3 w-full bg-indigo opacity-50 shadow duration-300`}
-        >
+      <div
+      className={` ${
+        open ? "lg:w-40" : "lg:w-60 "
+      } flex flex-col lg:h-screen p-3 w-full bg-black shadow duration-300`}
+    >
           <div className="space-y-3">
             <div className="flex items-center justify-between">
               <button onClick={() => setOpen(!open)}>
