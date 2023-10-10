c558421daa7586e49848b4429a6e8470d3fe894f
jwbnw <32151160+jwbnw@users.noreply.github.com>
2023-10-10 11:39:47 -0700

    #27: Cleans up t2a svc

--- src/services/TextToArtService.ts
+++ src/services/TextToArtService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-import GenerateTextToArtTransactionRequest from "models/GenerateTextToArtTransactionRequest";
-import { TextToArtRequest } from "models/TextToArtRequest";
+import TransactionDetail from "models/TransactionDetail";
+import { TextToArtTranscationRequest } from "models/TextToArtTranscationRequest";
 
 //TODO: This should probably be a class
 // TODO: Routes should constants and pre-defined. Domains should be in .env file.
@@ -11,46 +11,19 @@ const customConfig = {
   },
 };
 
-export async function GenerateStablityTextToArt(
-  request: TextToArtRequest
-): Promise<any> {
-  //still need to make response obj
-  try {
-    /*
-    const response = await axios.post<any>(
-      "https://localhost:7247/api/proxy/text-to-art/generate-stablity",
-      JSON.stringify(request),
-      customConfig
-    );
-    */
-    console.log(
-      "Spoofing the TextToArt.GenerateStablityTextToArt api call: ",
-      request
-    );
-    const result = true; //response.data;
-    return result;
-  } catch (err) {
-    console.log("Error in TextToArt.GenerateStablityTextToArt:", err);
-  }
-}
-
 export async function GenerateTextToArtTransaction(
-  request: TextToArtRequest
+  request: TextToArtTranscationRequest
 ): Promise<any> {
   //still need to make response obj
   try {
-    /*
+    
       const response = await axios.post<any>(
-        "https://localhost:7247/api/proxy/text-to-art/generate-stablity",
+        "https://localhost:7247/api/text-to-art/create-transaction",
         JSON.stringify(request),
         customConfig
       );
-      */
-    console.log(
-      "Spoofing the TextToArt.GenerateTextToArtTransaction api call: ",
-      request
-    );
-    const result = true; //response.data;
+      
+    const result = response.data;
     return result;
   } catch (err) {
     console.log("Error in TextToArt.GenerateTextToArtTransaction:", err);
