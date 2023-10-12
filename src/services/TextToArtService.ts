87f5cd91aaaeb04ed5cf73cce8273fdebd4a04a2
jwbnw <32151160+jwbnw@users.noreply.github.com>
2023-10-11 22:54:42 -0700

    #27: Cleans up service

--- src/services/TextToArtService.ts
+++ src/services/TextToArtService.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import TransactionDetail from "models/TransactionDetail";
 import { TextToArtTranscationRequest } from "models/TextToArtTranscationRequest";
+import { TextToArtTranscationResponse } from "models/TextToArtTranscationResponse";
+
 
 //TODO: This should probably be a class
 // TODO: Routes should constants and pre-defined. Domains should be in .env file.
@@ -11,19 +13,19 @@ const customConfig = {
   },
 };
 
-export async function GenerateTextToArtTransaction(
+export async function GenerateTextToArtResult(
   request: TextToArtTranscationRequest
-): Promise<any> {
-  //still need to make response obj
+): Promise<TextToArtTranscationResponse> {
   try {
     
-      const response = await axios.post<any>(
+      const response = await axios.post<TextToArtTranscationResponse>(
         "https://localhost:7247/api/text-to-art/create-transaction",
         JSON.stringify(request),
         customConfig
       );
       
     const result = response.data;
+    console.log("Loggin Response.Data TextToArt.GenerateTextToArtTransaction:", result);
     return result;
   } catch (err) {
     console.log("Error in TextToArt.GenerateTextToArtTransaction:", err);
